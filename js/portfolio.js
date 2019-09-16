8c8f041c636aa8b8dd3b8d150f2444a4a0fc9308
sainsh <kfsainsh@gmail.com>
2019-09-16 11:42:22 +0200

    added 3 text files for portfolio added a rule to the css for portfolio added a javascript file for reading to portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,28 @@
+var texts = ["consentcoin","gameOfLife","onlineBattleship"];
+var i;
+var textFile;
+var allText;
+
+for (i = 0; i < texts.length; i++) {
+    readFile(texts[i]);
+
+}
+
+function readFile(fileName) {
+
+    var textFile = new XMLHttpRequest();
+    var allText = "";
+    textFile.onreadystatechange = function () {
+        if (textFile.readyState === XMLHttpRequest.DONE && textFile.status == 200) {
+            allText = textFile.responseText;
+            allText = allText.split("\n").join("<br>");
+        }
+
+        document.getElementById(fileName).innerHTML = "<p>" + allText + "</p>";
+
+
+    }
+    textFile.open("GET", "text/" + fileName + ".txt", true);
+    textFile.send(null);
+
+}
