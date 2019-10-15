bbd3aee39c7ef751db83bab263a0ee17045902d1
sainsh <kfsainsh@gmail.com>
2019-10-15 11:14:28 +0200

    reworked on live site, commit to github to have backup changed pathfinder.js to use fetch instead of xhttp changed pathfinder.html to only show 1 character

--- js/pathfinder.js
+++ js/pathfinder.js
@@ -1,28 +1,15 @@
-var texts = ["world", "lasse", "stoffer", "hp", "kath", "marcus"];
+var texts = ["world", "lasse", "stoffer", "marcus"];
+var textRemains =["hp", "kath"];
 var i;
 var textFile;
 var allText;
 
 for (i = 0; i < texts.length; i++) {
-    readFile(texts[i]);
+    readFileFetch(texts[i]);
 
 }
 
-function readFile(fileName) {
-
-    var textFile = new XMLHttpRequest();
-    var allText = "";
-    textFile.onreadystatechange = function () {
-        if (textFile.readyState === XMLHttpRequest.DONE && textFile.status == 200) {
-            allText = textFile.responseText;
-            allText = allText.split("\n").join("<br>");
-        }
-
-        document.getElementById(fileName).innerHTML = "<p>" + allText + "</p>";
-
-
-    }
-    textFile.open("GET", "text/" + fileName + ".txt", true);
-    textFile.send(null);
-
+function readFileFetch(fileName){
+    fetch(`./text/${fileName}.txt`).then(res => res.text())
+    .then((data) => document.getElementById(fileName).innerHTML = `<p>${data}</p>`);
 }
